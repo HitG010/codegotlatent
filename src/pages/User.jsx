dca6cf5efd9ea75581d27d24999e43bdb980bae1
HitG010 <hiteshgupta_23ec091@dtu.ac.in>
2025-06-14 00:50:20 +0530

    User Profile API

---
+++ src/pages/User.jsx
@@ -0,0 +1,6 @@
+// import { getUserData } from "../api/api";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+
+// function User() {
+//     const { userId } = useParams();
