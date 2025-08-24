03ba84aeb69e62bb80d36bad877e58fffa285e32
kartikbindra <kartikbindra_23cs211@dtu.ac.in>
2025-08-25 01:14:14 +0530

    hui hui

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -32,12 +32,12 @@ app.use(express.json({ limit: "50mb" })); // Increase the limit to handle larger
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
+    // origin: "*",
     origin: [
       process.env.CLIENT_URL,
       `${process.env.CLIENT_URL}/auth`,
       "https://www.codegotlatent.com",
-      "http://localhost:5173"
+      "https://localhost:5173"
     ],
     credentials: true,
   })
